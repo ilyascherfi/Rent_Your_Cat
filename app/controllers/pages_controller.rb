0f5c9547e615675182efa41367186f8a1255d426
Hiedjoung O <hiedjoung.o@gmail.com>
2023-08-21 16:41:06 +0200

    sharing with ruth

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
+
   def home
   end
+
+  def destroy
+    
+    redirect_to  pages_path
+  end
+
 end
