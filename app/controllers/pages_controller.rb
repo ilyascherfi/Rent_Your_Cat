0b2063ad36b20faa0567511b99876d6fd8faa961
Ruth ITOUA GATSE <ruthitoua@yahoo.fr>
2023-08-21 17:16:14 +0200

    creation model cats

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
-
   def home
   end
 
   def destroy
-    
     redirect_to  pages_path
   end
 
