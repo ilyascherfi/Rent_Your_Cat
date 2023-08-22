f5fc4dfb9f2118caa2b2c12ac5a91e7e42de027d
Hiedjoung O <hiedjoung.o@gmail.com>
2023-08-22 15:12:51 +0200

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,67 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "json"
+require "open-uri"
+require 'faker'
+cat_breeds = [
+  "Abyssinian",
+  "American Shorthair",
+  "Bengal",
+  "British Shorthair",
+  "Maine Coon",
+  "Persian",
+  "Ragdoll",
+  "Siamese",
+  "Sphynx",
+  "Scottish Fold",
+  "Russian Blue",
+  "Savannah",
+  "Burmese",
+  "Himalayan",
+  "Turkish Van",
+  "Norwegian Forest Cat",
+  "Devon Rex",
+  "Cornish Rex",
+  "Exotic Shorthair",
+  "Somali"
+]
+
 
 puts 'Destroying all'
 User.destroy_all
 Cat.destroy_all
 Reservation.destroy_all
 
-puts 'Creating user'
-user = User.create(first_name: 'Thib', last_name: 'Chss', email: 'thib@gmail.com', password: 'azerty', address: 'Paris')
+puts 'Creating 50 users'
+50.times do
+  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 'azerty', address: Faker::Address.full_address, image_url: Faker::Avatar.image)
+  puts user.first_name
+end
+puts ''
+puts '50 users created'
 
-puts 'Creating cat'
-cat = Cat.create(user: User.first, name: 'Croquette', age: 2, sexe: 'Male', race: 'Bingual', location: 'Paris', description: 'Il est pas fun', price: 15)
+puts 'Creating 100 cats'
+100.times do
+  url = 'https://api.thecatapi.com/v1/images/search'
+  cat_serialized = URI.open(url).read
+  cat = JSON.parse(cat_serialized)
+  image_url = cat[0]["url"]
+  cat_user = User.all.sample
+  cat = Cat.create!(user: cat_user, name: Faker::Creature::Animal.name, age: rand(1..20), sexe: ['male', 'female'].sample, race: cat_breeds.sample, location: cat_user.address, description: Faker::Lorem.paragraph, price: rand(5..50), image_url: image_url)
+  puts cat.name
+end
+puts ''
+puts '100 cats created'
 
-puts 'Creating date'
-start_date = Date.today
-end_date = start_date + 3
+puts 'Creating 50 resa'
+today = Date.today
 
-puts 'Creating resa'
-reservation = Reservation.create!(user: User.first, cat: Cat.first, start_date: start_date, end_date: end_date, statut: false, total_price: 50)
+50.times do
+  random_days = rand(0..30)
+  random_date = today + random_days
+  reservation = Reservation.create!(user: User.all.sample, cat: Cat.all.sample, start_date: random_date, end_date: (random_date + 3), statut: false, total_price: 50)
+  puts reservation.cat.name
+end
+puts ''
+puts '50 resa created'
