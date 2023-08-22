32ff350b1014407fa18f8983f107294cff24ed28
Ilyas CHERFI <ilyascherfi91@gmail.com>
2023-08-22 15:14:03 +0200

    modification sur les controllers cats  et reservations, et model user

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,29 @@
 class ReservationsController < ApplicationController
+  def new
+    @reservation = Reservation.new
+    @cat = Cat.find(params[:cat_id])
+  end
+
+  def create
+    @cat = Cat.find(params[:cat_id])
+    @reservation = Reservation.new(reservation_params)
+    @reservation.cat = @cat
+    if @reservation.save
+      redirect_to @reservation, notice: "reservation was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to cats_path(@reservation.cat)
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:status, :start_date, :end_date, :total_price, :cat_id, :user_id)
+  end
 end
