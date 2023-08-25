41cd77e51c7f20065b6c8bcb9d6af76d38c9f21f
Hiedjoung O <hiedjoung.o@gmail.com>
2023-08-25 15:38:30 +0200

    popup done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,18 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-
     if params[:query].present?
       @cats = Cat.search_by_name_race_description_price_sexe_location(params[:query])
     else
       @cats = Cat.all
     end
+
     @users = User.all
     @markers = @cats.geocoded.map do |cat|
       {
         lat: cat.latitude,
-        lng: cat.longitude
+        lng: cat.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {cat: cat})
       }
     end
   end
@@ -21,5 +22,4 @@ class PagesController < ApplicationController
     @my_cats = current_user.cats
     @reservations = Reservation.where(cat: @my_cats)
   end
-
 end
